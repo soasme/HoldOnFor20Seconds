635ff1af6e6a3a29d5e0134e43facfb6f69e9260
soasme <soasme@gmail.com>
2016-09-16 20:44:44 +0800

    Init.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	public Vector2 force;
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		force = Vector2.left;
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce (force);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("die logic");
+		}
+		// Always destroy old bullet and respawn a new one,
+		// If old bullet detected collision.
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = gameObject.transform.position.x + Input.GetAxis ("Horizontal") * speed;
+		float y = gameObject.transform.position.y +Input.GetAxis ("Vertical") * speed;
+		x = Mathf.Clamp (x, -6.15f, 6.15f);
+		y = Mathf.Clamp (y, -4.5f, 4.5f);
+		Vector2 pos = new Vector2 (x, y);
+		gameObject.transform.position = pos;
+	}
+}
