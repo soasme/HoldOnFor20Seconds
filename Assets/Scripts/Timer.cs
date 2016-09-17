e735711d8ab93ede787a456ac4dce9b7b7ad9b40
soasme <soasme@gmail.com>
2016-09-17 19:54:14 +0800

    add timer.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public bool startTimer = true;
+	public float time = 0;
+	public float interval = 0.1f;
+	public TextMesh textMesh;
+
+	void Start () {
+		textMesh = GetComponent<TextMesh>();
+		Reset ();
+	}
+
+	void Reset () {
+		time = 0.0f;
+	}
+
+	IEnumerator Forward() {
+		yield return new WaitForSeconds (interval);
+		time += interval;
+		startTimer = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (startTimer) {
+			StartCoroutine ("Forward");
+			startTimer = false;
+		}
+		textMesh.text = time.ToString ("0.0");
+	}
+}
